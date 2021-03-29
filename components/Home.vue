b28ac57dd04a8d2218831e12a956de4f73c94199
mulugeta ewnu <mulugetaewnu@yahoo.com>
2021-03-29 14:48:09 +0300

    folder structure

---
+++ components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-container>
+        <v-row justify="space-around" align="center">
+            <v-col cols="2">
+            </v-col>
+            
+        </v-row>
+    </v-container>
+</template>
